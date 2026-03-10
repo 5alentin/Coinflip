faed911a66df644507b591417cc7ba1b6a2c43c5
5alentin <88522602+5alentin@users.noreply.github.com>
2026-03-10 10:15:32 -0400

    Add files via upload

---
+++ coinflip-sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'coinflip-v1';
+const ASSETS = [
+  '/coinflip/',
+  '/coinflip/index.html',
+  '/coinflip/manifest.json',
+  '/coinflip/icon-192.png',
+  '/coinflip/icon-512.png',
+  '/coinflip/apple-touch-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
